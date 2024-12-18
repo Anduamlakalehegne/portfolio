bb75fbfbbf3495b26423ffe64463c7b5e796f43e
Anduamlak Alehegne <anduamlakalehegne@gmail.com>
2024-12-18 22:59:02 +0300

    experiance data update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -6,12 +6,12 @@ import wegagen from '../assets/wegagen.png'
 
 export const experienceData = [
   {
-    role: "Frontend Developer Intern",
-    company: "ShowLife",
-    companyLogo: abronal,
-    date: "Feb 2022 - June 2022",
-    description: "As a Frontend Developer Intern, I had the opportunity to work closely with senior developers and contribute to the design, development, and optimization of web applications. I was involved in various stages of the development process, from prototyping to deploying interactive, responsive, and user-friendly interfaces.",
-    skills: ["HTML", "CSS", "JavaScript", ]
+    role: "Fullstack Developer",
+    company: "Wegagen Bank",
+    companyLogo: wegagen,
+    date: "March 2023 - Present",
+    description: "As a Fullstack Developer at Wegagen Bank, I worked on developing and maintaining both frontend and backend solutions for the bank's web applications. I utilized React and Node.js to create seamless user experiences while ensuring robust backend functionality. My role involved building scalable APIs, integrating third-party services, and optimizing application performance. Collaborating with cross-functional teams, I helped deliver secure, efficient, and user-friendly banking solutions that met business and customer needs.",
+    skills: ["ReactJS", "Redux", "TypeScript", "Next.Js", "Material UI", "NodeJs",, "Express.js", "Node.Js", "GraphQL", "PostgreSQL", "MongoDB", "Nginx"]
   },
   {
     role: "Frontend Developer",
@@ -22,11 +22,11 @@ export const experienceData = [
     skills: ["ReactJS", "Redux", "NodeJs", "Material UI", "HTML", "CSS", "JavaScript"]
   },
   {
-    role: "Fullstack Developer",
-    company: "Wegagen Bank",
-    companyLogo: wegagen,
-    date: "March 2023 - Present",
-    description: "As a Fullstack Developer at Wegagen Bank, I worked on developing and maintaining both frontend and backend solutions for the bank's web applications. I utilized React and Node.js to create seamless user experiences while ensuring robust backend functionality. My role involved building scalable APIs, integrating third-party services, and optimizing application performance. Collaborating with cross-functional teams, I helped deliver secure, efficient, and user-friendly banking solutions that met business and customer needs.",
-    skills: ["ReactJS", "Redux", "TypeScript", "Next.Js", "Material UI", "NodeJs",, "Express.js", "Node.Js", "GraphQL", "PostgreSQL", "MongoDB", "Nginx"]
+    role: "Frontend Developer Intern",
+    company: "ShowLife",
+    companyLogo: abronal,
+    date: "Feb 2022 - June 2022",
+    description: "As a Frontend Developer Intern, I had the opportunity to work closely with senior developers and contribute to the design, development, and optimization of web applications. I was involved in various stages of the development process, from prototyping to deploying interactive, responsive, and user-friendly interfaces.",
+    skills: ["HTML", "CSS", "JavaScript", ]
   }
 ];
