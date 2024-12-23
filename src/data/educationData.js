f62074cd67c40a2e5b98d5ba79a569cec2ed19b2
Anduamlak Alehegne <anduamlakalehegne@gmail.com>
2024-12-23 22:12:19 +0300

    .

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,7 +1,5 @@
 
-// import img from '../assets/main-img.png'
-// import abronal from '../assets/abronal.png'
-// import bazra from '../assets/bazra.png'
+
 import hawassa from '../assets/hawassa.jfif'
 
 export const educationData = [
@@ -13,12 +11,4 @@ export const educationData = [
     description: "Completed a Bachelor's degree in Information Technology from Hawassa University, focusing on developing skills in software development, database management, and IT infrastructure. The program included hands-on experience and in-depth knowledge of modern web technologies, programming languages, and network systems.",
     skills: ["Grade 3.54"]
   },
-  // {
-  //   role: "Frontend Developer",
-  //   company: "Bazra Technology Group",
-  //   companyLogo: hawassa,
-  //   date: "June 2022 - March 2023",
-  //   description: "At Bazra Technology Group, I contributed to developing responsive and user-friendly web applications. Using technologies like React, Vue.js, and CSS frameworks, I helped create dynamic interfaces while ensuring seamless performance across devices. I collaborated with cross-functional teams, integrated APIs, and optimized code for speed and efficiency, ensuring high-quality, user-centric solutions. This experience sharpened my skills in frontend development and solidified my passion for building intuitive web applications.",
-  //   skills: ["ReactJS", "Redux", "NodeJs", "Material UI", "HTML", "CSS", "JavaScript"]
-  // },
 ];
