d933a9d812df0cd9f71953fd42c02e86ac9df2d1
Anduamlak Alehegne <anduamlakalehegne@gmail.com>
2025-01-09 16:04:27 +0300

    Experiance Update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,32 +1,32 @@
 
 import img from '../assets/main-img.png'
-import abronal from '../assets/abronal.png'
+import minarc from '../assets/minarc.jpg'
 import bazra from '../assets/bazra.png'
 import wegagen from '../assets/wegagen.png'
 
 export const experienceData = [
   {
     role: "Fullstack Developer",
+    company: "Mender Information architecture and software design",
+    companyLogo: minarc,
+    date: "May 2024 - Dec 2024",
+    description: "As a Fullstack Developer at Mender Information architecture and software design, I built modular, scalable, and maintainable web applications using React, Next.js, and Node.js, while also developing and optimizing backend services by implementing RESTful APIs with secure authentication using JWT. Additionally, I designed and integrated efficient MySQL queries to enhance backend performance and ensure seamless data management.",
+    skills: ["ReactJS", "Redux", "TypeScript", "NextJS", "Tailwind CSS", "NestJS", "NodeJS", "ExpressJS", "GraphQL", "PostgreSQL", "MongoDB"]
+  },
+  {
+    role: "Full-Stack Developer",
     company: "Wegagen Bank",
     companyLogo: wegagen,
-    date: "March 2023 - Present",
-    description: "As a Fullstack Developer at Wegagen Bank, I worked on developing and maintaining both frontend and backend solutions for the bank's web applications. I utilized React and Node.js to create seamless user experiences while ensuring robust backend functionality. My role involved building scalable APIs, integrating third-party services, and optimizing application performance. Collaborating with cross-functional teams, I helped deliver secure, efficient, and user-friendly banking solutions that met business and customer needs.",
-    skills: ["ReactJS", "Redux", "TypeScript", "Next.Js", "Material UI", "NodeJs",, "Express.js", "Node.Js", "GraphQL", "PostgreSQL", "MongoDB", "Nginx"]
+    date: "Jun 2022 - Aug 2024",
+    description: "At Wegagen Bank, I engineered high-performance applications using React.js, Next.js, and NestJS for complex banking systems, collaborating with cross-functional teams to enhance UI/UX and ensure scalability in backend services with Node.js, PostgreSQL, and MongoDB. Additionally, I designed and implemented robust authentication solutions using NextAuth and JWT to ensure secure and seamless user experiences.",
+    skills: ["ReactJS", "Redux", "TypeScript", "NextJS", "Material UI", "NestJS", "NodeJS", "ExpressJS", "GraphQL", "PostgreSQL", "MYSQL", "Nginx"]
   },
   {
     role: "Frontend Developer",
     company: "Bazra Technology Group",
     companyLogo: bazra,
-    date: "June 2022 - March 2023",
-    description: "At Bazra Technology Group, I contributed to developing responsive and user-friendly web applications. Using technologies like React, Vue.js, and CSS frameworks, I helped create dynamic interfaces while ensuring seamless performance across devices. I collaborated with cross-functional teams, integrated APIs, and optimized code for speed and efficiency, ensuring high-quality, user-centric solutions. This experience sharpened my skills in frontend development and solidified my passion for building intuitive web applications.",
-    skills: ["ReactJS", "Redux", "NodeJs", "Material UI", "HTML", "CSS", "JavaScript"]
-  },
-  {
-    role: "Frontend Developer Intern",
-    company: "ShowLife",
-    companyLogo: abronal,
-    date: "Feb 2022 - June 2022",
-    description: "As a Frontend Developer Intern, I had the opportunity to work closely with senior developers and contribute to the design, development, and optimization of web applications. I was involved in various stages of the development process, from prototyping to deploying interactive, responsive, and user-friendly interfaces.",
-    skills: ["HTML", "CSS", "JavaScript", ]
+    date: "Jan 2022 - Aug 2022",
+    description: "As a Frontend Developer, I built and optimized responsive, user-friendly interfaces using React and TypeScript, while leading the development of a modular and scalable codebase to enhance maintainability and flexibility across the application.",
+    skills: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Redux" ]
   }
 ];
